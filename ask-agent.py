15b05d68a745fafea8aeaea51b3e4808f27604c6
cpitawanakwat <91224130+cpitawanakwat@users.noreply.github.com>
2023-06-30 12:23:12 -0500

    Add files via upload

---
+++ ask-agent.py
@@ -0,0 +1,73 @@
+import os
+
+# Set OpenAI API key
+with open('openai-key.txt', 'r') as f:
+    key = f.read().strip()
+
+os.environ['OPENAI_API_KEY'] = key
+
+from llm_prompt_creator import prompt as PR
+from langchain.tools import BaseTool
+from langchain.agents import initialize_agent
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+  
+class UnitTestGenerator(BaseTool):
+    name = "Unit Test Generator"
+    description = "use this tool when you need write unit test code."
+
+    def _run(self, folder_path: str):
+        PR.chunker(folder_path)
+        store = PR.create_store()
+        PR.prompt(store, show_context=False)
+        return "Unit test generation complete."
+    
+    def _arun(self, folder_path: str):
+        raise NotImplementedError("This tool does not support async")
+
+class CodeTestingTool(BaseTool):
+    name = "Code Tester"
+    description = "use this tool when you need to run the code for unit tests to determine if they pass for a given codebase."
+
+    def _run(self, folder_path: str):
+        return "Testing will occur here!"
+    
+    def _arun(self, folder_path: str):
+        raise NotImplementedError("This tool does not support async")
+
+
+# initialize LLM (we use ChatOpenAI because we'll later define a `chat` agent)
+llm = ChatOpenAI(
+    openai_api_key=os.environ['OPENAI_API_KEY'],
+    temperature=0,
+    model_name='gpt-3.5-turbo'
+)
+# initialize conversational memory
+conversational_memory = ConversationBufferWindowMemory(
+    memory_key='chat_history',
+    k=5,
+    return_messages=True
+)
+
+# new_prompt = agent.agent.create_prompt(
+#     system_message=sys_msg,
+#     tools=tools
+# )
+# agent.agent.llm_chain.prompt = new_prompt
+
+tools = [UnitTestGenerator(), CodeTestingTool()]
+
+# initialize agent with tools
+agent = initialize_agent(
+    agent='chat-zero-shot-react-description',
+    tools=tools,
+    llm=llm,
+    verbose=True,
+    max_iterations=3,
+    early_stopping_method='generate',
+    memory=conversational_memory
+)
+
+#agent("Can you write unit tests for the code in the folder called javafiles?")
+#agent("Can you test the code in the javafiles folder?")
+agent("Can you write unit tests for the code in the folder called javafiles, and then test the code in the javafiles folder?")
