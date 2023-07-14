5ffc8a562e7ec3ec1fe5ed36efa2e329d1b713e4
cpitawanakwat <91224130+cpitawanakwat@users.noreply.github.com>
2023-07-14 10:30:00 -0500

    add document search tool

--- ask-agent.py
+++ ask-agent.py
@@ -11,10 +11,17 @@ from langchain.tools import BaseTool
 from langchain.agents import initialize_agent
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.chat_models import ChatOpenAI
+from typing import Optional
+from langchain.text_splitter import CharacterTextSplitter
+import json
+from glob import glob
   
 class UnitTestGenerator(BaseTool):
     name = "Unit Test Generator"
-    description = "use this tool when you need write unit test code."
+    description = (
+        "use this tool when you need write unit test code for a software application. "
+        "only use this tool when explicitly asked to write test code for specific files."
+    )
 
     def _run(self, folder_path: str):
         PR.chunker(folder_path)
@@ -34,6 +41,36 @@ class CodeTestingTool(BaseTool):
     
     def _arun(self, folder_path: str):
         raise NotImplementedError("This tool does not support async")
+    
+class DocumentSearch(BaseTool):
+    name = "Documentation Search"
+    description = "use this tool when you need search through provided documentation for answers."
+
+    def _run(self, folder_path: str):
+        docs = [y for x in os.walk(folder_path) for y in glob(os.path.join(x[0], "*.txt"))]
+
+        data = []
+        for doc in docs:
+            rawText = open(doc, encoding = "ISO-8859-1").read()
+            data.append(rawText)
+
+        textSplitter = CharacterTextSplitter(chunk_size=2000, separator="\n")
+
+        docs = []
+        for sets in data:
+            docs.extend(textSplitter.split_text(sets))
+
+        #Create chunks.json file
+        with open("docs-search/store/chunks.json", 'w') as f:
+            json.dump(docs, f)
+
+        store = PR.create_store("docs-search/store/chunks.json", "docs-search/store/db")
+        PR.prompt(store, show_context=False)
+
+        return "Documentation search complete."
+    
+    def _arun(self, folder_path: str):
+        raise NotImplementedError("This tool does not support async")
 
 
 # initialize LLM (we use ChatOpenAI because we'll later define a `chat` agent)
@@ -42,6 +79,7 @@ llm = ChatOpenAI(
     temperature=0,
     model_name='gpt-3.5-turbo'
 )
+
 # initialize conversational memory
 conversational_memory = ConversationBufferWindowMemory(
     memory_key='chat_history',
@@ -55,11 +93,11 @@ conversational_memory = ConversationBufferWindowMemory(
 # )
 # agent.agent.llm_chain.prompt = new_prompt
 
-tools = [UnitTestGenerator(), CodeTestingTool()]
+tools = [UnitTestGenerator(), CodeTestingTool(), DocumentSearch()]
 
 # initialize agent with tools
 agent = initialize_agent(
-    agent='chat-zero-shot-react-description',
+    agent='zero-shot-react-description',
     tools=tools,
     llm=llm,
     verbose=True,
@@ -70,4 +108,7 @@ agent = initialize_agent(
 
 #agent("Can you write unit tests for the code in the folder called javafiles?")
 #agent("Can you test the code in the javafiles folder?")
-agent("Can you write unit tests for the code in the folder called javafiles, and then test the code in the javafiles folder?")
+#agent("Can you write unit tests for the code in the folder called javafiles, and then test the code in the javafiles folder?")
+#agent("What is the capital of China?")
+#agent("Can you look up the current weather in my area?")
+agent("Can you search the documentation in the folder called docs-search?")
