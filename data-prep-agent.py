a4f961bbf0fa73e75494321d92c612642c39a82f
cpitawanakwat <91224130+cpitawanakwat@users.noreply.github.com>
2023-08-15 09:51:21 -0500

    Update data-prep-agent.py

--- data-prep-agent.py
+++ data-prep-agent.py
@@ -11,14 +11,10 @@ from langchain.tools import BaseTool
 from langchain.agents import initialize_agent, load_tools
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.chat_models import ChatOpenAI
-from typing import Optional
-from langchain.text_splitter import CharacterTextSplitter
 import json
-from glob import glob
-import data_chunker.parser as file_parser
+#from glob import glob
 import openai
 import pandas as pd
-import csv
 import tiktoken
 import io
 import ast
@@ -28,18 +24,16 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 def askLLM(message, model="gpt-3.5-turbo-16k"):
-    messages = [
-    {"role": "user", "content": message},
-    ]
+    messages = [{"role": "user", "content": message},]
 
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0.1,
-        #stream=True
+        temperature=0,
     )
 
     return response["choices"][0]["message"]["content"]
+
 
 ########### Data Preparation Tools ###########
 from pydantic import BaseModel, BaseSettings, Field
@@ -90,42 +84,52 @@ class FindCategoryOpportunitiesTool(BaseTool, BaseSettings):
         raise NotImplementedError("This tool does not support async")
 
 
-CATEGORY_VALUES: str = Field(description="The category values that a free text column should be mapped to")
-COLUMN_NAME: str = Field(description="the name of the column that needs its text values mapped to categories")
+#CATEGORY_VALUES: str = Field(description="The category values that a free text column should be mapped to")
+COLUMN_NAME: list = Field(description="the names of the columns that need to be modified for data cleaning/preparation. use python list formatting.")
 
-class textToCatColumns(BaseModel):
+class standardToolArgs(BaseModel):
     csv_file: str = CSV_FILE
     #category_values: str = CATEGORY_VALUES
-    column_name: str = COLUMN_NAME
+    column_name: list = COLUMN_NAME
     
-class AssignCategoryValuesTool(BaseTool, BaseSettings):
-    name = "Assign Category Values Tool"
+class TextPreprocessingTool(BaseTool, BaseSettings):
+    name = "Text Preprocessing Tool"
     description = (
-        "Use this tool to assign category values in place of free text fields for the provided columns. "
-        # "If the user did not provide the category names that need to be mapped to the text values for the "
-        # "column name, prompt the user for those values."
+        "Use this tool for text preprocessing, specifically to convert free text fields into categorical values. "
+        "This tool should never be used to encode categorical values. It is only meant for text preprocessing to convert free text into categories."
     )
-    args_schema: Type[textToCatColumns] = textToCatColumns
+    args_schema: Type[standardToolArgs] = standardToolArgs
 
-    def _run(self, csv_file: str, column_name: str):
-        # Code to replace free text columns with categories
-        df = pd.read_csv(csv_file)
-        #df_new = pd.DataFrame(columns=column_name)
+    def _run(self, csv_file: str, column_name: list):
+        # check if updated csv file already exists, and if so use that file
+        substr = "."
+        idx = csv_file.index(substr)
+        updatedCSV = csv_file[:idx] + "_updated" + csv_file[idx:]
+
         try:
-            f = open("categoryValues.json")
-            jsonFile = json.load(f)
-            category_values = jsonFile[column_name]
+            df = pd.read_csv(updatedCSV)
         except:
-            return f"Please add category values for the {column_name} column in the categoryValues.json file and try again."
+            df = pd.read_csv(csv_file)
 
+        # check for category values to map free text to
+        if len(column_name) > 1:
+            return "error - need dev to handle multiple columns"
+        colName = column_name[0]
+
+        try:
+            f = open("datasetInfo.json")
+            jsonFile = json.load(f)
+            category_values = jsonFile["Category Values"][colName]
+        except:
+            return f"Please add category values for the {colName} column in the datasetInfo.json file and try again."
+
+        # split rows in the dataset into a smaller list of rows
         encoding = tiktoken.get_encoding("cl100k_base")
         dataList = []
         tempList = []
         i = 0
         while i < len(df):
-            row_to_append = df.iloc[i][column_name]
-
-            #df_new = pd.concat([df_new, row_to_append.to_frame().T], ignore_index=True)
+            row_to_append = df.iloc[i][colName]
             tempList.append(row_to_append)
             
             # Calculate current token count
@@ -133,34 +137,18 @@ class AssignCategoryValuesTool(BaseTool, BaseSettings):
             numTokens = len(encoding.encode(dfAsString))
 
             if (numTokens >= 1000) or (i == len(df)-1):
-                #dataList.append(df_new)
-                #df_new = pd.DataFrame(columns=df.columns)
                 dataList.append(tempList)
                 tempList = []
 
             i += 1
 
-        #print(len(dataList))
-
+        # loop through the list of the split dataset and query LLM for each
         fullList = []
         for partialDf in dataList:
             print(". ")
             x = pd.Series(partialDf)
             xLength = len(x)
-            #print(xLength)
-
-            # message = f'''
-            # Categorize the data in the following list into the following category values:\n
-            # {category_values}
-            # Only use those listed category values. Do not create any new values.
-            # \n
-            # Respond with all data points in the same order in this list format: ["item1", "item2", "item3"]
-            # The response should contain the same number of items as the data series provided below. 
-            # Therefore, the response list should contain {xLength} items.
-            # \n
-            # Data:\n
-            # {x}
-            # '''
+ 
             message = f'''
             Categorize the data in the data series below with the following category values:\n
             {category_values}
@@ -178,49 +166,127 @@ class AssignCategoryValuesTool(BaseTool, BaseSettings):
             newCol = askLLM(message, "gpt-4")
 
             newList = ast.literal_eval(newCol)
-            #print(len(newList))
             fullList = fullList + newList
-            #print(fullList)
 
-        df.drop(column_name, axis=1, inplace=True)
-        #df["Category"] = fullList
-        df[column_name] = fullList
+        # replace free text column with categorical encoded columns
+        df.drop(colName, axis=1, inplace=True)
+        df[colName] = fullList
 
         dummyList = []
-        dummyList.append(column_name)
+        dummyList.append(colName)
         updatedDf = pd.get_dummies(df, columns=dummyList)
-        updatedDf.to_csv("updatedFeedback.csv", index=False)
+        updatedDf.to_csv(updatedCSV, index=False)
 
-        return "complete"
+        return f"Updated dataset now saved in {updatedCSV}"
     
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async")
 
-class CreateDataPrepPlan(BaseTool):
+class CreateDataPrepPlanTool(BaseTool, BaseSettings):
     name = "Create Data Prep Plan"
     description = (
-        "Use this tool to create a step by step plan for cleaning/feature engineering a dataset for machine learning."
+        "Use this tool to create a step by step plan for cleaning/feature engineering a dataset for machine learning. "
+        "Always use this tool first when asked to prepare a dataset for machine learning models. "
+        "After determining the data preparation steps, use the tools you have available to perform those steps. "
+        "If there are no tools available for a specific step, skip that step and inform the user of what needs to be completed. "
+        "Then move to the next step."
     )
+    args_schema: Type[csvSchema] = csvSchema
 
-    def _run(self, folder_path: str):
-        df = pd.read_csv(folder_path)
-        data = df.head()
+    def _run(self, csv_file: str):
+        df = pd.read_csv(csv_file)
+        data = df.head(10)
         buf = io.StringIO()
         s = df.info(buf=buf)
         info = buf.getvalue()
+        descStats = df.describe()
+        corr = df.corr(numeric_only=True)
 
+        f = open("datasetInfo.json")
+        jsonFile = json.load(f)
+        targetVar = jsonFile["Column Descriptions"]["Target"]
+        dataDescription = jsonFile["Column Descriptions"]["Description"]
+        columnsDescription = jsonFile["Column Descriptions"]["Columns"]
+
+        # query LLM to determine list of columns to address
         message = f'''
-        What data preparation steps would you recommend for a machine learning model based on the following data summary
-        and first 5 row sample of the dataset?
+        Review the information on the dataset below and determine which data cleaning steps should be done in preparation for machine learning models.
+        Be specific in what steps should be completed.
+
+        Data Information:
+        The target variable for the dataset is {targetVar}.
+        {dataDescription}. The features are:
+        {columnsDescription}
 
         Data summary:
         {info}
 
         Data sample:
         {data}
+
+        Descriptive Statistics:
+        {descStats}
+
+        Correlation Matrix:
+        {corr}
         '''
 
-        return "test"
+        catInfoResponse = askLLM(message, model="gpt-4")
+        return catInfoResponse
+    
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
+
+
+class RemoveColumnsTool(BaseTool, BaseSettings):
+    name = "Remove Columns Tool"
+    description = (
+        "Use this tool to remove unneeded columns in a dataset for machine learning."
+    )
+    args_schema: Type[standardToolArgs] = standardToolArgs
+
+    def _run(self, csv_file: str, column_name: list):
+        substr = "."
+        idx = csv_file.index(substr)
+        updatedCSV = csv_file[:idx] + "_updated" + csv_file[idx:]
+
+        try:
+            df = pd.read_csv(updatedCSV)
+        except:
+            df = pd.read_csv(csv_file)
+        
+        for col in column_name:
+            df.drop(col, axis=1, inplace=True)
+
+        df.to_csv(updatedCSV, index=False)
+
+        return f"Successfully removed the following columns: {column_name}"
+    
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
+
+
+class EncodeCategoricalValuesTool(BaseTool, BaseSettings):
+    name = "Encode Categorical Values Tool"
+    description = (
+        "Use this tool encode categorical values."
+    )
+    args_schema: Type[standardToolArgs] = standardToolArgs
+
+    def _run(self, csv_file: str, column_name: list):
+        substr = "."
+        idx = csv_file.index(substr)
+        updatedCSV = csv_file[:idx] + "_updated" + csv_file[idx:]
+
+        try:
+            df = pd.read_csv(updatedCSV)
+        except:
+            df = pd.read_csv(csv_file)
+        
+        updatedDf = pd.get_dummies(df, columns=column_name)
+        updatedDf.to_csv(updatedCSV, index=False)
+
+        return f"Successfully encoded the following columns: {column_name}"
     
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async")
@@ -246,8 +312,11 @@ tools = load_tools(
     llm=llm,
 )
 
-tools.append(FindCategoryOpportunitiesTool())
-tools.append(AssignCategoryValuesTool())
+#tools.append(FindCategoryOpportunitiesTool())
+tools.append(CreateDataPrepPlanTool())
+tools.append(RemoveColumnsTool())
+tools.append(EncodeCategoricalValuesTool())
+tools.append(TextPreprocessingTool())
 
 # initialize agent with tools
 agent = initialize_agent(
@@ -262,7 +331,8 @@ agent = initialize_agent(
 
 # The below print statement will show the information that the agent is working with (tool descriptions, etc.)
 #print(agent.agent.llm_chain.prompt.messages[0].prompt.template)
-
+# query = input("prompt: ")
+# agent(query)
 agent("Can you prepare a dataset for machine learning models? Use sampleFeedback50.csv located in the datasets/ folder.")
 
 # test=['Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Housing and Transportation', 'Activities and Traveling', 'Health and Well-being Support', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Financial Support', 'Financial Support', 'Financial Support', 'Financial Support', 'Financial Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs']
@@ -299,3 +369,56 @@ agent("Can you prepare a dataset for machine learning models? Use sampleFeedback
 
 # print(dataList)
 # print(len(dataList))
+
+
+
+
+
+# csv_file = "datasets/feedback1.csv"
+# df = pd.read_csv(csv_file)
+# data = df.head(10)
+# buf = io.StringIO()
+# s = df.info(buf=buf)
+# info = buf.getvalue()
+# descStats = df.describe()
+# corr = df.corr(numeric_only=False)
+
+# f = open("datasetInfo.json")
+# jsonFile = json.load(f)
+# targetVar = jsonFile["Column Descriptions"]["Target"]
+# dataDescription = jsonFile["Column Descriptions"]["Description"]
+# columnsDescription = jsonFile["Column Descriptions"]["Columns"]
+
+# # query LLM to determine list of columns to address
+# message = f'''
+# Review the information on the dataset below and determine which data cleaning steps should be done in preparation for machine learning models.
+# Be specific in what steps should be completed.
+
+# After determining the data preparation steps, use the tools you have available to perform those steps. If there are no tools available for a
+# specific step, inform the user of what needs to be completed.
+
+# Data Information:
+# The target variable for the dataset is {targetVar}.
+# {dataDescription}. The features are:
+# {columnsDescription}
+
+# Data summary:
+# {info}
+
+# Data sample:
+# {data}
+
+# Descriptive Statistics:
+# {descStats}
+
+# Correlation Matrix:
+# {corr}
+# '''
+
+# print(message)
+# encoding = tiktoken.get_encoding("cl100k_base")
+# print(len(encoding.encode(message)))
+
+
+# catInfoResponse = askLLM(message, model="gpt-4")
+# print(catInfoResponse)
