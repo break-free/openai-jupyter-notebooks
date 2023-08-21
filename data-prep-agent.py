dc15a45b9c38a72e186a1c5c078f8601f5cd6861
cpitawanakwat <91224130+cpitawanakwat@users.noreply.github.com>
2023-08-21 15:53:48 -0500

    Update data-prep-agent.py

--- data-prep-agent.py
+++ data-prep-agent.py
@@ -12,7 +12,6 @@ from langchain.agents import initialize_agent, load_tools
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.chat_models import ChatOpenAI
 import json
-#from glob import glob
 import openai
 import pandas as pd
 import tiktoken
@@ -39,18 +38,24 @@ def askLLM(message, model="gpt-3.5-turbo-16k"):
 from pydantic import BaseModel, BaseSettings, Field
 from typing import Type
 CSV_FILE: str = Field( description="the csv file name and/or location")
+COLUMN_NAME: list = Field(description="the names of the columns that need to be modified for data cleaning/preparation. use python list formatting.")
 
 class csvSchema(BaseModel):
     csv_file: str = CSV_FILE
 
-class FindCategoryOpportunitiesTool(BaseTool, BaseSettings):
-    name = "Find Category Opportunities Tool"
+class standardToolArgs(BaseModel):
+    csv_file: str = CSV_FILE
+    column_name: list = COLUMN_NAME
+
+
+class CreateDataPrepPlanTool(BaseTool, BaseSettings):
+    name = "Create Data Prep Plan"
     description = (
-        "Use this tool as the first step when you need to prepare/clean/feature engineer a dataset for "
-        "machine learning models or algorithms. This tool will be used to determine if any columns should "
-        "be converted to categorical values, and then perform those converstions. If the user did not provide "
-        "all arguments required to use this tool, use the human tool to get that information first. Never "
-        "pass in a null value, and never use a value that wasn't provided directly by the user. "
+        "Use this tool to create a step by step plan for cleaning/feature engineering a dataset for machine learning. "
+        "Always use this tool first when asked to prepare a dataset for machine learning models. "
+        "After determining the data preparation steps, use the tools you have available to perform those steps. "
+        "If there are no tools available for a specific step, skip that step and inform the user of what needs to be completed. "
+        "Then move to the next step and use the tools to complete those if available."
     )
     args_schema: Type[csvSchema] = csvSchema
 
@@ -60,38 +65,46 @@ class FindCategoryOpportunitiesTool(BaseTool, BaseSettings):
         buf = io.StringIO()
         s = df.info(buf=buf)
         info = buf.getvalue()
-        
+        descStats = df.describe()
+        corr = df.corr(numeric_only=True)
+
+        f = open("datasetInfo.json")
+        jsonFile = json.load(f)
+        targetVar = jsonFile["Column Descriptions"]["Target"]
+        dataDescription = jsonFile["Column Descriptions"]["Description"]
+        columnsDescription = jsonFile["Column Descriptions"]["Columns"]
+
         # query LLM to determine list of columns to address
         message = f'''
-        The following data summary and data sample are from a dataset that will be used for machine learning models.
-        Review the columns and determine if there are free text columns that could be converted into categorical values.
-        Do not include free text columns that are already broken down into categories.
+        Review the information on the dataset below and determine which data cleaning steps should be done in preparation for machine learning models.
+        Be specific in what steps should be completed.
+        If there is a column that should appear in multiple steps that may be conflicting, include it only in the step that makes the most sense for that column.
 
-        Respond only with the names of all relevant columns in the following format: ["column 1", "column 2", "column 3"]
+        Data Information:
+        The target variable for the dataset is {targetVar}.
+        {dataDescription}. The features are:
+        {columnsDescription}
 
         Data summary:
         {info}
 
         Data sample:
         {data}
+
+        Descriptive Statistics:
+        {descStats}
+
+        Correlation Matrix:
+        {corr}
         '''
 
         catInfoResponse = askLLM(message, model="gpt-4")
-
         return catInfoResponse
     
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async")
 
 
-#CATEGORY_VALUES: str = Field(description="The category values that a free text column should be mapped to")
-COLUMN_NAME: list = Field(description="the names of the columns that need to be modified for data cleaning/preparation. use python list formatting.")
-
-class standardToolArgs(BaseModel):
-    csv_file: str = CSV_FILE
-    #category_values: str = CATEGORY_VALUES
-    column_name: list = COLUMN_NAME
-    
 class TextPreprocessingTool(BaseTool, BaseSettings):
     name = "Text Preprocessing Tool"
     description = (
@@ -139,6 +152,7 @@ class TextPreprocessingTool(BaseTool, BaseSettings):
             if (numTokens >= 1000) or (i == len(df)-1):
                 dataList.append(tempList)
                 tempList = []
+                print("added list of rows")
 
             i += 1
 
@@ -178,61 +192,6 @@ class TextPreprocessingTool(BaseTool, BaseSettings):
         updatedDf.to_csv(updatedCSV, index=False)
 
         return f"Updated dataset now saved in {updatedCSV}"
-    
-    def _arun(self, query: str):
-        raise NotImplementedError("This tool does not support async")
-
-class CreateDataPrepPlanTool(BaseTool, BaseSettings):
-    name = "Create Data Prep Plan"
-    description = (
-        "Use this tool to create a step by step plan for cleaning/feature engineering a dataset for machine learning. "
-        "Always use this tool first when asked to prepare a dataset for machine learning models. "
-        "After determining the data preparation steps, use the tools you have available to perform those steps. "
-        "If there are no tools available for a specific step, skip that step and inform the user of what needs to be completed. "
-        "Then move to the next step."
-    )
-    args_schema: Type[csvSchema] = csvSchema
-
-    def _run(self, csv_file: str):
-        df = pd.read_csv(csv_file)
-        data = df.head(10)
-        buf = io.StringIO()
-        s = df.info(buf=buf)
-        info = buf.getvalue()
-        descStats = df.describe()
-        corr = df.corr(numeric_only=True)
-
-        f = open("datasetInfo.json")
-        jsonFile = json.load(f)
-        targetVar = jsonFile["Column Descriptions"]["Target"]
-        dataDescription = jsonFile["Column Descriptions"]["Description"]
-        columnsDescription = jsonFile["Column Descriptions"]["Columns"]
-
-        # query LLM to determine list of columns to address
-        message = f'''
-        Review the information on the dataset below and determine which data cleaning steps should be done in preparation for machine learning models.
-        Be specific in what steps should be completed.
-
-        Data Information:
-        The target variable for the dataset is {targetVar}.
-        {dataDescription}. The features are:
-        {columnsDescription}
-
-        Data summary:
-        {info}
-
-        Data sample:
-        {data}
-
-        Descriptive Statistics:
-        {descStats}
-
-        Correlation Matrix:
-        {corr}
-        '''
-
-        catInfoResponse = askLLM(message, model="gpt-4")
-        return catInfoResponse
     
     def _arun(self, query: str):
         raise NotImplementedError("This tool does not support async")
@@ -292,6 +251,150 @@ class EncodeCategoricalValuesTool(BaseTool, BaseSettings):
         raise NotImplementedError("This tool does not support async")
 
 
+class HandleOutliersTool(BaseTool, BaseSettings):
+    name = "Handle Outliers Tool"
+    description = (
+        "Use this tool to handle outlier values in a dataset. If the concern is handling negative values, use the Handle Negative Values Tool instead."
+    )
+    args_schema: Type[standardToolArgs] = standardToolArgs
+
+    def _run(self, csv_file: str, column_name: list):
+        substr = "."
+        idx = csv_file.index(substr)
+        updatedCSV = csv_file[:idx] + "_updated" + csv_file[idx:]
+
+        try:
+            df = pd.read_csv(updatedCSV)
+        except:
+            df = pd.read_csv(csv_file)
+        
+        # keep track of which columns get updated
+        changedCols = []
+        notChangedCols = []
+        
+        for col in column_name:
+            # given the context, ask LLM how it would handle the outliers
+            data = df.head(10)
+            buf = io.StringIO()
+            s = df.info(buf=buf)
+            info = buf.getvalue()
+            descStats = df.describe()
+
+            bins = pd.cut(df[col], bins=20, include_lowest=True)
+            bucket_counts = bins.value_counts().sort_index()
+
+            # skew of a normal distribution is -1 to 1, so anything far outside this range indicates big outlier
+            skew = df[col].skew()
+
+            f = open("datasetInfo.json")
+            jsonFile = json.load(f)
+            targetVar = jsonFile["Column Descriptions"]["Target"]
+            dataDescription = jsonFile["Column Descriptions"]["Description"]
+            columnsDescription = jsonFile["Column Descriptions"]["Columns"]
+
+            # In the future the LLM should determine between different options for how to handle outliers.
+            # Right now, it's not good enough at determining the best option based on the business context.
+            # - Replace outliers with the median (median)
+            # - Remove outliers from the dataset (trim)
+            # - Floor and cap the outliers to the lower and upper quartiles (floor and cap)
+            # - Do nothing because the outliers are valid in the business context (keep)
+            
+            message = f'''
+            Which values should be considered outliers and adjusted to the median in the '{col}' column?
+            Use the data set information below to help with this determination. Make the best decision based on the data available.
+
+            Respond with two numbers: 1 low number for which everything equal to or below that value should be adjusted to the median, and
+            1 high number for which everything equal to or above that value should be adjusted to the median. Also provide an explanation for the decision.
+
+            Respond with this format: ['low number', 'high number', 'explanation']
+            If there are no outliers on a given end of the data, reply with 'N/A' instead of a number.
+            Remember, any number you provide for the low/high number is to be considered inclusive for median adjustment.
+
+            Data Information:
+            The target variable for the dataset is {targetVar}.
+            {dataDescription}. The features are:
+            {columnsDescription}
+
+            Data summary:
+            {info}
+
+            Data sample:
+            {data}
+
+            Descriptive Statistics:
+            {descStats}
+
+            Column Distribution for {col}:
+            {bucket_counts}
+
+            Skew of {col}: {skew}
+            '''
+            result = askLLM(message, "gpt-4")
+            print(result)
+            response = ast.literal_eval(result)
+
+            # get the numbers for below and above which need to be handled as outliers
+            try:
+                lowNum = ast.literal_eval(response[0]) #if number is returned, need the correct type
+            except:
+                lowNum = response[0]
+            try:
+                highNum = ast.literal_eval(response[1]) #if number is returned, need the correct type
+            except:
+                highNum = response[1]
+            
+            # for now always use median; in future this should be dynamic based on the LLM response
+            median = df[col].median()
+
+            if lowNum == 'N/A' and highNum != 'N/A':
+                df[col] = df[col].apply(lambda x: median if x >= highNum else x)
+            elif lowNum != 'N/A' and highNum == 'N/A':
+                df[col] = df[col].apply(lambda x: median if x <= lowNum else x)
+            elif lowNum != 'N/A' and highNum != 'N/A':
+                df[col] = df[col].apply(lambda x: median if (x <= lowNum) or (x >= highNum) else x)
+
+            # write updated file and add column to the appropriate list to be tracked as changed or not
+            if lowNum != 'N/A' or highNum != 'N/A':
+                df.to_csv(updatedCSV, index=False)
+                changedCols.append(col)
+            else:
+                notChangedCols.append(col)
+
+        return f"Successfully handled outliers for the following columns: {changedCols}. No changes made for the following columns: {notChangedCols}."
+    
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
+
+
+class HandleNegativeValuesTool(BaseTool, BaseSettings):
+    name = "Handle Negative Values Tool"
+    description = (
+        "Use this remove negative values from columns when those negative values don't make sense in the business context."
+    )
+    args_schema: Type[standardToolArgs] = standardToolArgs
+
+    def _run(self, csv_file: str, column_name: list):
+        substr = "."
+        idx = csv_file.index(substr)
+        updatedCSV = csv_file[:idx] + "_updated" + csv_file[idx:]
+
+        try:
+            df = pd.read_csv(updatedCSV)
+        except:
+            df = pd.read_csv(csv_file)
+        
+        for col in column_name:
+            median = df[col].median()
+            df[col] = df[col].apply(lambda x: median if x < 0 else x)
+
+        df.to_csv(updatedCSV, index=False)
+
+        return "Negative values successfully replaced."
+    
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
+    
+
 ########### Set up LLM and Agent ###########
 llm = ChatOpenAI(
     openai_api_key=os.environ['OPENAI_API_KEY'],
@@ -312,11 +415,12 @@ tools = load_tools(
     llm=llm,
 )
 
-#tools.append(FindCategoryOpportunitiesTool())
 tools.append(CreateDataPrepPlanTool())
+tools.append(TextPreprocessingTool())
 tools.append(RemoveColumnsTool())
 tools.append(EncodeCategoricalValuesTool())
-tools.append(TextPreprocessingTool())
+tools.append(HandleOutliersTool())
+tools.append(HandleNegativeValuesTool())
 
 # initialize agent with tools
 agent = initialize_agent(
@@ -331,57 +435,23 @@ agent = initialize_agent(
 
 # The below print statement will show the information that the agent is working with (tool descriptions, etc.)
 #print(agent.agent.llm_chain.prompt.messages[0].prompt.template)
+
 # query = input("prompt: ")
 # agent(query)
-agent("Can you prepare a dataset for machine learning models? Use sampleFeedback50.csv located in the datasets/ folder.")
 
-# test=['Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Housing and Transportation', 'Activities and Traveling', 'Health and Well-being Support', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Academic Support and Resources', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Athletics and sports', 'Athletics and sports', 'Athletics and sports', 'Career opportunities', 'Career opportunities', 'Career opportunities', 'Financial Support', 'Financial Support', 'Financial Support', 'Financial Support', 'Financial Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'Health and Well-being Support', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'International student experience', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Online learning', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs', 'Student Affairs']
-# print(len(test))
-
-
-#Academic Support and Resources, Athletics and sports, Career opportunities, Financial Support  
+agent("Can you prepare a dataset for machine learning models? Use Bank_Personal_Loan.csv located in the datasets/ folder.")
+#agent("Can you preprocess text in datasets/feedback1.csv and convert the Reports column into categories?")
 
 
-# df = pd.read_csv("datasets/feedback1.csv")
-# df_new = pd.DataFrame(columns=["Reports"])
-
-# encoding = tiktoken.get_encoding("cl100k_base")
-# dataList = []
-# tempList = []
-# i = 0
-# while i < len(df):
-#     row_to_append = df.iloc[i]["Reports"]
-
-#     #df_new = pd.concat([df_new, row_to_append.to_frame().T], ignore_index=True)
-#     tempList.append(row_to_append)
-    
-#     # Calculate current token count
-#     dfAsString = f"{tempList}"
-#     numTokens = len(encoding.encode(dfAsString))
-
-#     if (numTokens >= 2000) or (i == len(df)-1):
-#         #dataList.append(df_new)
-#         #df_new = pd.DataFrame(columns=df.columns)
-#         dataList.append(tempList)
-#         tempList = []
-
-#     i += 1
-
-# print(dataList)
-# print(len(dataList))
-
-
-
-
-
-# csv_file = "datasets/feedback1.csv"
-# df = pd.read_csv(csv_file)
+# df = pd.read_csv("datasets/Bank_Personal_Loan.csv")
 # data = df.head(10)
 # buf = io.StringIO()
 # s = df.info(buf=buf)
 # info = buf.getvalue()
 # descStats = df.describe()
-# corr = df.corr(numeric_only=False)
+
+# #skew of a normal distribution is -1 to 1, so anything far outside this range indicates big outlier
+# print(df['Experience'].skew())
 
 # f = open("datasetInfo.json")
 # jsonFile = json.load(f)
@@ -389,36 +459,8 @@ agent("Can you prepare a dataset for machine learning models? Use sampleFeedback
 # dataDescription = jsonFile["Column Descriptions"]["Description"]
 # columnsDescription = jsonFile["Column Descriptions"]["Columns"]
 
-# # query LLM to determine list of columns to address
-# message = f'''
-# Review the information on the dataset below and determine which data cleaning steps should be done in preparation for machine learning models.
-# Be specific in what steps should be completed.
 
-# After determining the data preparation steps, use the tools you have available to perform those steps. If there are no tools available for a
-# specific step, inform the user of what needs to be completed.
-
-# Data Information:
-# The target variable for the dataset is {targetVar}.
-# {dataDescription}. The features are:
-# {columnsDescription}
-
-# Data summary:
-# {info}
-
-# Data sample:
-# {data}
-
-# Descriptive Statistics:
-# {descStats}
-
-# Correlation Matrix:
-# {corr}
-# '''
-
-# print(message)
-# encoding = tiktoken.get_encoding("cl100k_base")
-# print(len(encoding.encode(message)))
-
-
-# catInfoResponse = askLLM(message, model="gpt-4")
-# print(catInfoResponse)
+# bins = pd.cut(df["Experience"], bins=20, include_lowest=True)
+# bucket_counts = bins.value_counts().sort_index()
+# print(bucket_counts)
+# print(df["Experience"].value_counts())
